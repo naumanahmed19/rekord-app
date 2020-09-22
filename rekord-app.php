b657f709f135cc96960a0f04257a5f1b08f48b83
Nauman <naumanahmed19@gmail.com>
2020-09-22 13:01:45 +0200

    first commit

---
+++ rekord-app.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Plugin Name: Rekord App
+ * Description: A must have plugin to enhance Rekord theme functionality.
+ * Plugin URI:  https://xvelopers.com/
+ * Version:     1.0.0
+ * Author:      Nomi
+ * Author URI:  https://xvelopers.com/
+ * Text Domain: rekord
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+final class Rekord_App {
+
+	/**
+	 * Plugin Version
+	 *
+	 * @since 1.2.0
+	 * @var string The plugin version.
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.2.0
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function __construct() {
+
+		// Load translation
+		add_action( 'init', array( $this, 'i18n' ) );
+
+		// Init Plugin
+		add_action( 'plugins_loaded', array( $this, 'init' ) );
+	}
+
+	/**
+	 * Load Textdomain
+	 *
+	 * Load plugin localization files.
+	 * Fired by `init` action hook.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function i18n() {
+		load_plugin_textdomain( 'rekord' );
+	}
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * Validates that Elementor is already loaded.
+	 * Checks for basic plugin requirements, if one check fail don't continue,
+	 * if all check have passed include the plugin class.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 */
+	public function init() {
+
+		if( ! class_exists( 'XV_Updater' ) ){
+			include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+			$updater = new XV_Updater( __FILE__ );
+			$updater->set_username( 'naumanahmed19' );
+			$updater->set_repository( 'rekord-app' );
+			/*
+				$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+			*/
+			$updater->initialize();
+	
+		}
+
+
+		require_once( __DIR__ . '/api/api.php' );
+
+	}
+}
+new Rekord_App();
